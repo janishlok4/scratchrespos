bbb0c100ec8e253e6fae0aaaf29417d90b236370
Shlok Jani <janishlok4@gmail.com>
2021-02-02 17:01:00 -0500

    commit

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let quotes = [
 -Levi Ackermann from Attack on Titan`,
 `"I have never tried poop before."
 -My little brother`,
+`"Is water wet?"
+-My little brother`,
 ]
 
-let index = Math.floor(Math.random() * 2)
+let index = Math.floor(Math.random() * 3)
 console.log(quotes[index])
